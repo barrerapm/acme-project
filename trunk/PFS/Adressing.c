face39cc12cd18521cf27486f3934c6095913824
barrerapm@gmail.com <barrerapm@gmail.com@1c5c8d8d-2960-b2c9-1d80-9b2c365f9ad2>
2011-10-19 07:43:34 +0000

    Capa direccionamiento

---
+++ trunk/PFS/Adressing.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "PFS.h"
+#include "PPD.h"
+#include "Adressing.h"
+
+extern ConfigPFS config_pfs; // Estructura global config_ppd
+
+void Adressing_read_block(BootSector* bs, Block* block, int n_block) {
+	Cluster* cluster = (Cluster*) malloc(sizeof(Cluster));
+	int i;
+	for (i = 0; i < config_pfs.clusters_per_block; i++) {
+		Adressing_read_cluster(bs, cluster, n_block * config_pfs.clusters_per_block + i);
+		block->cluster[i] = *cluster;
+	}
+}
+
+void Adressing_read_cluster(BootSector* bs, Cluster* cluster, int n_cluster) {
+	Sector* sector = (Sector*) malloc(sizeof(Sector));
+	int i, first_DataRegion_sector = bs->reserved_sector_count + bs->number_of_fats * bs->fat32_sectors_per_fat;
+	int n_sector = first_DataRegion_sector + n_cluster * config_pfs.sectors_per_cluster;
+	for (i = 0; i < config_pfs.sectors_per_cluster; i++, n_sector++) {
+		PPD_read_sector(sector, n_sector);
+		cluster->sector[i] = *sector;
+	}
+}
