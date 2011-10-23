a5acbe488e2c1d474fab259f508f61b55904e090
barrerapm@gmail.com <barrerapm@gmail.com@1c5c8d8d-2960-b2c9-1d80-9b2c365f9ad2>
2011-10-23 23:19:31 +0000

    FAT.h

--- trunk/PFS/FAT.h
+++ trunk/PFS/FAT.h
@@ -4,6 +4,8 @@
 struct boot_sector;
 struct FAT_table;
 
-void FAT_get_file_clusters(struct boot_sector*, struct FAT_table*, int*);
+void FAT_get_file_clusters(int*);
+int FAT_get_free_clusters(int , int* );
+void FAT_write_FAT_entry(int , int* );
 
 #endif /* FAT_H_ */
