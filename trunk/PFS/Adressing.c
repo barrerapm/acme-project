42f815bb767aed8297394537cb3a876ca77511af
barrerapm@gmail.com <barrerapm@gmail.com@1c5c8d8d-2960-b2c9-1d80-9b2c365f9ad2>
2011-10-23 23:16:52 +0000

    Adressing.c

--- trunk/PFS/Adressing.c
+++ trunk/PFS/Adressing.c
@@ -7,16 +7,18 @@
 
 extern ConfigPFS config_pfs; // Estructura global config_ppd
 
-void Adressing_read_block(BootSector* bs, Block* block, int n_block) {
+void Adressing_read_block(Block* block, int n_block) {
+	extern BootSector* bs;
 	Cluster* cluster = (Cluster*) malloc(sizeof(Cluster));
 	int i;
 	for (i = 0; i < config_pfs.clusters_per_block; i++) {
-		Adressing_read_cluster(bs, cluster, n_block * config_pfs.clusters_per_block + i);
+		Adressing_read_cluster(cluster, n_block * config_pfs.clusters_per_block + i);
 		block->cluster[i] = *cluster;
 	}
 }
 
-void Adressing_read_cluster(BootSector* bs, Cluster* cluster, int n_cluster) {
+void Adressing_read_cluster(Cluster* cluster, int n_cluster) {
+	extern BootSector* bs;
 	Sector* sector = (Sector*) malloc(sizeof(Sector));
 	int i, first_DataRegion_sector = bs->reserved_sector_count + bs->number_of_fats * bs->fat32_sectors_per_fat;
 	int n_sector = first_DataRegion_sector + n_cluster * config_pfs.sectors_per_cluster;
@@ -26,3 +28,10 @@ void Adressing_read_cluster(BootSector* bs, Cluster* cluster, int n_cluster) {
 	}
 }
 
+void Adressing_close_disk(void){
+	PPD_close_disk();
+}
+
+int Adressing_open_disk(void){
+	return PPD_open_disk();
+}
