ed2f99636bd55bf3c93b8609e1f5ea03e1b54026
barrerapm@gmail.com <barrerapm@gmail.com@1c5c8d8d-2960-b2c9-1d80-9b2c365f9ad2>
2011-10-23 23:17:21 +0000

    Adressing.h

--- trunk/PFS/Adressing.h
+++ trunk/PFS/Adressing.h
@@ -3,7 +3,10 @@
 
 struct boot_sector;
 
-void Adressing_read_cluster(struct boot_sector*, struct cluster*, int);
-void Adressing_read_block(struct boot_sector*, struct block*, int);
+void Adressing_read_cluster(struct cluster*, int);
+void Adressing_read_block(struct block*, int);
+void Adressing_close_disk(void);
+int Adressing_open_disk(void);
+
 
 #endif /* ADRESSING_H_ */
