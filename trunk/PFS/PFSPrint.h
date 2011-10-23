c46a765a4c9e86f968863180dc3ae839f8d2afb7
barrerapm@gmail.com <barrerapm@gmail.com@1c5c8d8d-2960-b2c9-1d80-9b2c365f9ad2>
2011-10-23 23:21:52 +0000

    PFSPrint.h

--- trunk/PFS/PFSPrint.h
+++ trunk/PFS/PFSPrint.h
@@ -11,14 +11,15 @@ struct long_dir_entry;
 struct dir_entry;
 
 // Prints para pruebas
-void PFSPrint_fs_info(struct fs_info*);
-void PFSPrint_FAT_table(struct FAT_table);
-void PFSPrint_bootsector(struct boot_sector*);
-void PFSPrint_block(struct boot_sector*, struct block, int);
-void PFSPrint_cluster(struct boot_sector*, struct cluster, int);
+void PFSPrint_fs_info(void);
+void PFSPrint_FAT_table(void);
+void PFSPrint_bootsector(void);
+void PFSPrint_block(struct block, int);
+void PFSPrint_cluster(struct cluster, int);
 void PFSPrint_sector(struct sector, int);
 void PFSPrint_long_dir_entry(struct long_dir_entry*);
-void PFSPrint_dir_entry(struct dir_entry*);
+void PFSPrint_short_dir_entry(struct dir_entry*);
+void PFSPrint_dir_entry(int, struct long_dir_entry*, struct dir_entry*);
 void ln(void);
 
 #endif /* PFSPrint_H_ */
