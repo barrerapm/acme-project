8d69f10bdd16d147d0e7e21d38ddd7c3aa8c6264
barrerapm@gmail.com <barrerapm@gmail.com@1c5c8d8d-2960-b2c9-1d80-9b2c365f9ad2>
2011-10-23 23:21:14 +0000

    PFS.h

--- trunk/PFS/PFS.h
+++ trunk/PFS/PFS.h
@@ -9,87 +9,88 @@ typedef struct config_pfs {
 } ConfigPFS;
 
 struct boot_sector {
-	u_char jump_instruction[3];
-	u_char oem_name[8];
-	unsigned short bytes_per_sector;
-	unsigned char sectors_per_cluster;
-	unsigned short reserved_sector_count;
-	unsigned char number_of_fats;
-	unsigned short root_directory_location;
-	unsigned short total_sectors;
-	unsigned char media_descriptor;
-	unsigned short sectors_per_fat;
-	unsigned short sectors_per_track;
-	unsigned short number_of_heads;
-	unsigned int count_of_hidden_sectors;
-	unsigned int total_sectors2;
+	uint8_t jump_instruction[3];
+	uint8_t oem_name[8];
+	uint16_t bytes_per_sector;
+	uint8_t sectors_per_cluster;
+	uint16_t reserved_sector_count;
+	uint8_t number_of_fats;
+	uint16_t root_directory_location;
+	uint16_t total_sectors;
+	uint8_t media_descriptor;
+	uint16_t sectors_per_fat;
+	uint16_t sectors_per_track;
+	uint16_t number_of_heads;
+	uint32_t count_of_hidden_sectors;
+	uint32_t total_sectors2;
 	// Fat32
-	unsigned int fat32_sectors_per_fat;
-	unsigned short fat32_flags;
-	unsigned short fat32_version;
-	unsigned int fat32_cluster_num_dir_start;
-	unsigned short fat32_sector_num_fs_inf_sec;
-	unsigned short fat32_sector_num_copy_bs;
-	u_char fat32_reserved[12];
-	u_char fat32_physical_drive_num;
-	u_char fat32_reserved_12_16;
-	u_char fat32_ext_boot_signature;
-	unsigned int fat32_serial_num2;
-	u_char fat32_volume_label[11];
-	u_char fat32_file_system_type[8];
-	u_char fat32_os_boot_code32[420];
-	unsigned short fat32_boot_sector_signature;
+	uint32_t fat32_sectors_per_fat;
+	uint16_t fat32_flags;
+	uint16_t fat32_version;
+	uint32_t fat32_first_cluster;
+	uint16_t fat32_sector_num_fs_inf_sec;
+	uint16_t fat32_sector_num_copy_bs;
+	uint8_t fat32_reserved[12];
+	uint8_t fat32_physical_drive_num;
+	uint8_t fat32_reserved_12_16;
+	uint8_t fat32_ext_boot_signature;
+	uint32_t fat32_serial_num2;
+	uint8_t fat32_volume_label[11];
+	uint8_t fat32_file_system_type[8];
+	uint8_t fat32_os_boot_code32[420];
+	uint16_t fat32_boot_sector_signature;
 	// Fat1216
-	u_char fat1216_physical_drive_number;
-	u_char fat1216_current_head;
-	u_char fat1216_ext_boot_signature;
-	unsigned int fat1216_serial_num;
-	u_char fat1216_volume_label[11];
-	u_char fat1216_file_system_type[8];
-	u_char fat1216_os_boot_code1216[448];
-	unsigned short fat1216_boot_sector_signature;
+	uint8_t fat1216_physical_drive_number;
+	uint8_t fat1216_current_head;
+	uint8_t fat1216_ext_boot_signature;
+	uint32_t fat1216_serial_num;
+	uint8_t fat1216_volume_label[11];
+	uint8_t fat1216_file_system_type[8];
+	uint8_t fat1216_os_boot_code1216[448];
+	uint16_t fat1216_boot_sector_signature;
 }__attribute__((packed));
 
 struct fs_info {
-	u_char sector_signature[4];
-	u_char reserved[480];
-	u_char sector_signature2[4];
-	unsigned int free_clusters;
-	unsigned int most_recently_alloc_cluster;
-	u_char reserved2[12];
-	u_char sector_signature3[4];
+	uint8_t sector_signature[4];
+	uint8_t reserved[480];
+	uint8_t sector_signature2[4];
+	uint32_t free_clusters;
+	uint32_t most_recently_alloc_cluster;
+	uint8_t reserved2[12];
+	uint8_t sector_signature3[4];
 }__attribute__((packed));
 
 struct FAT_table {
-	unsigned int entry[130816];
+	uint32_t entry[130816];
 };
 
 struct dir_entry {
-	char dos_file_name[8];
-	char dos_file_extension[3];
-	u_char file_attributes;
-	u_char reserved;
-	u_char create_time;
-	unsigned short create_time2;
-	unsigned short create_date;
-	unsigned short last_acces_date;
-	unsigned short fst_cluster_high;
-	unsigned short last_modified_time;
-	unsigned short last_modified_date;
-	unsigned short fst_cluster_low;
-	unsigned int file_size;
+	int8_t dos_file_name[8];
+	int8_t dos_file_extension[3];
+	uint8_t file_attributes;
+	uint8_t reserved;
+	uint8_t create_time;
+	uint16_t create_time2;
+	uint16_t create_date;
+	uint16_t last_acces_date;
+	uint16_t fst_cluster_high;
+	uint16_t last_modified_time;
+	uint16_t last_modified_date;
+	uint16_t fst_cluster_low;
+	uint32_t file_size;
 }__attribute__((packed));
 
 struct long_dir_entry {
-	u_char sequence_number;
-	unsigned short name1[5];
-	u_char attributes;
-	u_char reserved;
-	u_char checksum;
-	unsigned short name2[6];
-	unsigned short first_cluster_n;
-	unsigned short name3[2];
+	uint8_t sequence_number;
+	uint16_t name1[5];
+	uint8_t attributes;
+	uint8_t reserved;
+	uint8_t checksum;
+	uint16_t name2[6];
+	uint16_t first_cluster_n;
+	uint16_t name3[2];
 }__attribute__((packed));
+
 
 struct sector;
 struct cluster;
@@ -101,14 +102,22 @@ typedef struct fs_info FSInfo;
 typedef struct dir_entry DirEntry;
 typedef struct long_dir_entry LongDirEntry;
 
-void PFS_file_list(BootSector*);
-void PFS_directory_list(BootSector*, TablaFAT*, const char*, char**, int*);
-void PFS_rename_file(BootSector*, TablaFAT*, const char*, const char*);
-void PFS_read_file_content(BootSector*, TablaFAT*, const char*);
-int PFS_search_file_entry(BootSector*, DirEntry*, LongDirEntry*, const char*);
-char* PFS_file_name(DirEntry*, LongDirEntry*);
-void PFS_load_entry(struct cluster*, DirEntry*, LongDirEntry*, int);
-int PFS_max_cluster_entries(BootSector*);
-int PFS_n_first_file_cluster(DirEntry*);
-int PFS_total_file_clusters(BootSector*, DirEntry*);
+void PFS_file_list(void);
+void PFS_rename_file(const char*, const char*);
+int32_t PFS_directory_list(const char*, char**, int*);
+void PFS_read_file_content(const char*);
+int32_t PFS_search_path_entry(DirEntry* dir_entry, LongDirEntry* long_dir_entry, const char* path);
+int32_t PFS_search_file_entry(DirEntry*, LongDirEntry*, const char*,int);
+void PFS_path_dir_names(const char* , char**,int*);
+char* PFS_file_name(int attr, LongDirEntry* , DirEntry* );
+char* PFS_long_file_name(LongDirEntry*);
+char* PFS_short_file_name(DirEntry*);
+int32_t PFS_load_entry(struct cluster* , LongDirEntry* , DirEntry* , int );
+void PFS_load_long_entry(struct cluster*,LongDirEntry*, int);
+void PFS_load_short_entry(struct cluster*, DirEntry*, int);
+int32_t PFS_max_cluster_entries(void);
+int32_t PFS_n_first_file_cluster(DirEntry*);
+int32_t PFS_total_file_clusters(DirEntry*);
+void PFS_inicializar_config(void);
+
 #endif /* PFS_H_ */
