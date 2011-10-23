4513b8df950eb975576a93bd12e03d0f92603cc8
barrerapm@gmail.com <barrerapm@gmail.com@1c5c8d8d-2960-b2c9-1d80-9b2c365f9ad2>
2011-10-23 23:18:52 +0000

    Disk.h

--- trunk/PFS/Disk.h
+++ trunk/PFS/Disk.h
@@ -2,7 +2,7 @@
 #define DISK_H_
 
 void Disk_load_bootsector(BootSector*);
-void Disk_load_FSInfo(BootSector*, FSInfo*);
-void Disk_load_FAT(BootSector*, TablaFAT*);
+void Disk_load_FSInfo(FSInfo*);
+void Disk_load_FAT(TablaFAT*);
 
 #endif /* DISK_H_ */
