e261631bcd927d8036a764a147c359dd48f4eb2f
barrerapm <barrerapm@1c5c8d8d-2960-b2c9-1d80-9b2c365f9ad2>
2011-12-28 23:14:48 +0000

    Borro las cosas

--- trunk/PFS/Disk.h
+++
@@ -1,8 +0,0 @@
-#ifndef DISK_H_
-#define DISK_H_
-
-void Disk_load_bootsector(BootSector*);
-void Disk_load_FSInfo(FSInfo*);
-void Disk_load_FAT(TablaFAT*);
-
-#endif /* DISK_H_ */
--- trunk/PRAID/PRAID1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int mainPRAID(void){
-  // Definir
-  return 0;
-}
